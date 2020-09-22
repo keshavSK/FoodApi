6fe5723628b21e80904cf39ace9e7bfb99ed3e73
Keshav Katad <kkatad123@gmail.com>
2020-09-22 10:08:26 +0530

    remove constant variables

--- app/src/main/java/com/elitizamaty/foodapp/utils/Constant.kt
+++ app/src/main/java/com/elitizamaty/foodapp/utils/Constant.kt
@@ -2,8 +2,6 @@ package com.elitizamaty.foodapp.utils
 
 class Constant {
     companion object {
-        const val FOOD_API_BASE_URL = "https://api.spoonacular.com/"
-        const val FOOD_API_KEY = "30e937618f664b7b90683104b85e56ee"
         const val DUMMY_BASE_URL = "https://reqres.in/"
     }
 }
